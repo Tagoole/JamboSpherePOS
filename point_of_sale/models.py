6f3babffa70db7373b242ae645d496e8585fc39e
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-04-18 01:52:58 +0300

    Set up the html pages

--- point_of_sale/models.py
+++ point_of_sale/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.utils import timezone
+from django.db.models import Sum
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ["name"]
+        
+        
+
+class Sale(models.Model):
+    sale_date = models.DateTimeField(default=timezone.now)
+    total_amount = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        default=0.00,
+        editable=False
+    )
+    
+    def __str__(self):
+        return f"Sale #{self.id} - {self.sale_date.strftime('%Y-%m-%d %H:%M')} - UGX {self.total_amount:,}"
+    
+    class Meta:
+        ordering= ["-sale_date"]
+        
+        
+    def update_total(self):
+        total = self.items.aggregate(total=Sum("subtotal"))["total"] or 0
+        self.total_amount = total
+        self.save(update_fields=["total_amount"])
+        
+        
+        
+        
+
+class SaleItem(models.Model):
+    sale = models.ForeignKey(Sale,on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    unit_price = 
