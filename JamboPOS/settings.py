b757376a20a0657924db6645c91d600ec46af0ce
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-04-18 10:36:46 +0300

    Added the app of pwa to installed apps

--- JamboPOS/settings.py
+++ JamboPOS/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "point_of_sale"
+    "point_of_sale",
+    "pwa"
 ]
 
 MIDDLEWARE = [
