a4270ac9ea0a0e35ad5990febe797e20ff494339
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-04-22 06:42:19 +0300

    Batch 1: 1 files

--- point_of_sale/models.py
+++ point_of_sale/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
