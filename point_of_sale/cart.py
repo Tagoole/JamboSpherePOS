61021b1dfdbf4a18135df7590dffd29063538487
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-04-23 22:08:38 +0300

    The sales page is now working but the finish and pay is now the issue

--- point_of_sale/cart.py
+++ point_of_sale/cart.py
@@ -1,6 +1,6 @@
-from decimal import Decimal
 from django.conf import settings
 from .models import Product
+import copy
 
 class Cart:
     def __init__(self, request):
@@ -13,7 +13,7 @@ class Cart:
     def add(self, product, quantity=1, override_quantity=False):
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
+            self.cart[product_id] = {'quantity': 0, 'price': float(product.price)}
         if override_quantity:
             self.cart[product_id]['quantity'] = quantity
         else:
@@ -32,12 +32,12 @@ class Cart:
     def __iter__(self):
         product_ids = self.cart.keys()
         products = Product.objects.filter(id__in=product_ids)
-        cart = self.cart.copy()
+        cart = copy.deepcopy(self.cart)
         for product in products:
             cart[str(product.id)]['product'] = product
 
         for item in cart.values():
-            item['price'] = Decimal(item['price'])
+            item['price'] = float(item['price'])
             item['total_price'] = item['price'] * item['quantity']
             yield item
 
@@ -45,7 +45,7 @@ class Cart:
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
-        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+        return sum(float(item['price']) * item['quantity'] for item in self.cart.values())
 
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
